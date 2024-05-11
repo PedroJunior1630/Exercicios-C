9d9768aae53573692b36ebd63d8a9b5649d54e67
PedroHDev <85499142+PedroJunior1630@users.noreply.github.com>
2024-05-10 21:30:34 -0300

    Atualizar o ex080.c

--- Exercicios-com-C/ex080.c
+++ Exercicios-com-C/ex080.c
@@ -3,8 +3,8 @@ int main()
 {
 	float r, comp;
 	float pi = 3.1415;
-	printf("Digite o raio do circulo(m): ");
+	printf("Digite o raio do circulo(m²): ");
 	scanf("%f",&r);
 	comp = 2 * pi * r;
-	printf("O comprimento deste ciruclo é de %.4fm",comp);
+	printf("O comprimento deste ciruclo é de %.4fπm²",comp);
 }
