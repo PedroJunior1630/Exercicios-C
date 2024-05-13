3eaf34a2cda3aa6287b407189e556a5f73d717aa
pedrojunior <ph751153@gmail.com>
2024-05-13 20:46:47 -0300

    ex096

--- Exercicios-com-C/ex096.c
+++ Exercicios-com-C/ex096.c
@@ -22,41 +22,58 @@ void menu(){
     cabecalho("ESCOLHA UMA OPÇÃO");
     printf("[ 1 ] - Probabilidade de sair um número par\n");
     printf("[ 2 ] - Probabilidade de sair um número ímpar\n");
+    printf("[ 3 ] - Probabilidade sair um número múltiplo de 3\n");
     linha(30);
 }
 
 int main(){
     cabecalho("PROBABILIDADE DE CONJUNTOS");
+
     int tam,opc, maior;
     float pa;
     float par = 0;
     float impar = 0;
+    float multi = 0;
     int conjunto[32];
+
     printf("Digite o tamanho do conjunto: ");
     scanf("%d",&tam);
+
     for(int c=1; c<=tam;c++){
         printf("Digite o número inteiro da posição %d do conjunto: ",c);
         scanf("%d",&conjunto[c]);
         if(conjunto[c] % 2 == 0){
             par += 1;
-        }else{
+        }
+        if(conjunto[c] % 2 ==1){
             impar += 1;
         }
+        if(conjunto[c] % 3 ==0){
+            multi += 1;
+        }
     }
+
     linha(60);
     printf("Seu conjunto tem %d valores e sendo eles: ",tam);
+
     for(int c=1;c<=tam;c++){
         printf("|  %d  | ",conjunto[c]);
     }
+
     printf("\n");
     menu();
+
     printf("Qual opção deseja? ");
     scanf("%d",&opc);
+
     if(opc == 1){
         pa = (par/tam) * 100;
     }
     if(opc == 2){
         pa = (impar/tam) * 100;
+    }
+    if(opc == 3){
+        pa = (multi/tam) * 100;
     }
     printf("A probabilidade do conjunto é P(a) = %.2f por cento.",pa);
     return 0;
