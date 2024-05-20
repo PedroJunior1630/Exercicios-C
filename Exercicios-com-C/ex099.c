4b678e8d971bdc4b6006f3fe1d1191016b692ae1
pedrojunior <ph751153@gmail.com>
2024-05-20 15:48:26 -0300

    ex099

--- Exercicios-com-C/ex099.c
+++ Exercicios-com-C/ex099.c
@@ -1,47 +1,21 @@
 #include <stdio.h>
-#include <string.h>
 #include <time.h>
 
-void linha(int tam){
-    int c;
-    while(c<=tam){
-        printf("=");
+int main(){
+    srand(time(NULL));
+    int nums[4];
+    int c, sorteio;
+    int somapar=0;
+    printf("O 5 números sorteados foram...\n");
+    while(c<5){
+        sorteio = rand() % 10 + 1;
+        nums[c] = sorteio;
+        if(nums[c] % 2 == 0){
+            somapar += nums[c];
+        }
+        printf("| %d | ",nums[c]);
         c++;
     }
-    printf("\n");
-}
-
-void cabecalho(char msg[]){
-    int tam;
-    tam = strlen(msg) + 4;
-    linha(tam);
-    printf("  %s\n",msg);
-    linha(tam);
-}
-
-int main(){
-    cabecalho("JUJUTSU KAISEN : GAME");
-    printf("Se prepare para a luta...\n")
-    printf("Seu personagem é o SATORO GOJO... Veja sobre: \n");
-    cabecalho("SATORO GOJO");
-    printf("1° Técnica: Intangibilidade(\n");
-    printf("2° Técnica: Seis olhos \n");
-    printf("3° Técnica: Azul \n");
-    printf("4° Técnica: Vermelho\n");
-    printf("5° Técnica: Vazio Roxo \n");
-    printf("6° Técnica: Vazio Imensurável\n");
-    printf("7° Técnica: Domínio Simples: \n");
-    printf("8° Técnica: Reversão do fetiço\n");
-    linha(40);
-    printf("Observe seu adversário Ryohmen Sukuna... Veja sobre: \n");
-    cabecalho("SUKUNA");
-    printf("1° Técnica: Corte Normal\n");
-    printf("2° Técnica: Amplificação de domínio\n");
-    printf("3° Técnica: Santuário Malevolente\n");
-    printf("4° Técnica: Mahoraga\n");
-    printf("5° Técnica: Agito\n");
-    printf("6° Técnica: Desmantelar \n");
-    printf("7° Técnica: Reversão do fetiço\n");
-    linha(40);
+    printf("Somando apenas os números pares: %d\n",somapar);
     return 0;
 }
