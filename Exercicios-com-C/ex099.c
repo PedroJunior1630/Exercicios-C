1664dfe2a9bccd9a580b6a04318706ee75743a84
pedrojunior <ph751153@gmail.com>
2024-05-20 14:39:31 -0300

    ex099 cabecalho

--- Exercicios-com-C/ex099.c
+++ Exercicios-com-C/ex099.c
@@ -11,10 +11,23 @@ void linha(int tam, int tipo){
         if(tipo == 2){
             printf("=-=");
         }
-        if(tipo ==3){
+        if(tipo == 3){
             printf("-=-");
         }
         c++;
     }
     printf("\n");
+}
+
+void cabecalho(char palavra[]){
+    linha(20, 3);
+    printf("    %s",palavra);
+    linha(20, 3);
+}
+
+int main(){
+    linha(20,3);
+    cabecalho("JUJUTSU KAISEN : GAME\n");
+    personagens
+    return 0;
 }
