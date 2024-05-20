42ed2b907664c263ee2af63637b56516a798bbac
pedrojunior <ph751153@gmail.com>
2024-05-20 15:01:01 -0300

    ex099 infosukuna

--- Exercicios-com-C/ex099.c
+++ Exercicios-com-C/ex099.c
@@ -24,13 +24,24 @@ int main(){
     printf("Se prepare para a luta...\n")
     printf("Seu personagem é o SATORO GOJO... Veja sobre: \n");
     cabecalho("SATORO GOJO");
-    printf("1° Técnica: Intangibilidade(Mugen)\n");
-    printf("2° Técnica: Seis olhos(Rikugan)\n");
-    printf("3° Técnica: Azul(amplificação do fetiço)\n");
-    printf("4° Téncica: Vermelho(Reversão do fetiço dentro do mugen)\n");
-    printf("5° Técnica: Vazio Roxo(Azul+Vermelho)\n");
+    printf("1° Técnica: Intangibilidade(\n");
+    printf("2° Técnica: Seis olhos \n");
+    printf("3° Técnica: Azul \n");
+    printf("4° Técnica: Vermelho\n");
+    printf("5° Técnica: Vazio Roxo \n");
+    printf("6° Técnica: Vazio Imensurável\n");
+    printf("7° Técnica: Domínio Simples: \n");
+    printf("8° Técnica: Reversão do fetiço\n");
+    linha(40);
     printf("Observe seu adversário Ryohmen Sukuna... Veja sobre: \n");
     cabecalho("SUKUNA");
-
+    printf("1° Técnica: Corte Normal\n");
+    printf("2° Técnica: Amplificação de domínio\n");
+    printf("3° Técnica: Santuário Malevolente\n");
+    printf("4° Técnica: Mahoraga\n");
+    printf("5° Técnica: Agito\n");
+    printf("6° Técnica: Desmantelar \n");
+    printf("7° Técnica: Reversão do fetiço\n");
+    linha(40);
     return 0;
 }
